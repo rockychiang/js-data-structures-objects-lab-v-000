51fb85c7594f92a1dbe180736cbecae17c8076fa
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-01 03:31:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,5 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-  driver[key] = value;
-  return driver
+  return driver[key] = value;
 }

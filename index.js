38513877d9432559528bf113dddbaa2677b70c33
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-01 03:30:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  const newDriver = Object.assign({}, driver);
-  newDriver[key] = value;
+  const newDriver = Object.assign({}, driver, {key: value});
   return newDriver
 }
 
